68e7072ed35cfd9bc41cc74b9772403038a8f55f
Lendon <ranierlendon11@gmail.com>
2026-01-16 21:38:21 +0800

    fix: paddle getting out of screen

--- Game2_Breakout/Assets/Scripts/PlayerControl.cs
+++ Game2_Breakout/Assets/Scripts/PlayerControl.cs
@@ -21,5 +21,9 @@ public class PlayerControl : MonoBehaviour
     void FixedUpdate()
     {
         rb.linearVelocity = speed * Time.fixedDeltaTime * move;
+        
+        // Prevents player from getting out of screen
+        float clampedX = Mathf.Clamp(rb.position.x, -7.9f, 7.9f);
+        rb.position = new Vector2(clampedX, rb.position.y);
     }
 }
